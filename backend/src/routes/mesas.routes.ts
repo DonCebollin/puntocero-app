463e4a5aae26a885af3a90764ef8fc5f7d9f1668
DonCebollin <cristopher.alarcon2201@alumnos.ubiobio.cl>
2026-09-18 04:15:54 -0300

    feat: agrega middleware de autenticacion y protege rutas de mesas

--- backend/src/routes/mesas.routes.ts
+++ backend/src/routes/mesas.routes.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { Server } from "socket.io";
 import { crearMesasController } from "../controller/mesas.controller";
-
+import { verificarToken, verificarRol } from "../middleware/auth.middleware";
 export function mesasRoutes(io: Server): Router {
     const router = Router();
     const controller = crearMesasController(io);
 
-    router.get("/", controller.listar);
-    router.patch("/:id/estado", controller.actualizarEstado);
-    router.post("/", controller.crear);
-    router.delete("/:id", controller.eliminar);
+    router.get("/", verificarToken, controller.listar);
+    router.patch("/:id/estado", verificarToken, controller.actualizarEstado);
+    router.post("/", verificarToken, verificarRol("administrador"),controller.crear);
+    router.delete("/:id", verificarToken, verificarRol("administrador"), controller.eliminar);
     
     return router;
 }
