cd411c30824474bd1b9c5f116e5a5029f34444bf
DonCebollin <cristopher.alarcon2201@alumnos.ubiobio.cl>
2026-09-26 01:26:47 -0300

    chore: fuerza nueva compilacion del frontend (cache limpia)

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,6 +1,7 @@
 import { io, Socket} from "socket.io-client";
 import { BACKEND_URL } from "./config";
 
+//Conexion socket.io
 export const socket: Socket = io(BACKEND_URL || undefined, {
     autoConnect: true,
     reconnection: true,
